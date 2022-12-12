8833092aabb04c097d80d6fe6938894b67ff8aa5
brendanjcarlson <brendancarlson1125@gmail.com>
2022-12-12 04:42:14 -0600

    as done as it will be

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -10,3 +10,19 @@ def home():
     pokemon = [Pokemon.query.filter_by(game_id=1).first(), Pokemon.query.filter_by(
         game_id=4).first(), Pokemon.query.filter_by(game_id=7).first()]
     return render_template('home.html.j2', user=current_user, pokemon=pokemon)
+
+
+@app.route('/search/', methods=['GET', 'POST'])
+@login_required
+def search():
+    name = request.form['name']
+    print(name)
+    pokemon = Pokemon.query.filter_by(name=name).first()
+    return render_template('single.html.j2', user=current_user, pokemon=pokemon)
+
+
+@app.route('/pokemon/<int:game_id>', methods=['GET', 'POST'])
+@login_required
+def pokemon(game_id):
+    pokemon = Pokemon.query.filter_by(game_id=game_id).first()
+    return render_template('single.html.j2', user=current_user, pokemon=pokemon)
--- app/blueprints/user/routes.py
+++ app/blueprints/user/routes.py
@@ -1,16 +1,31 @@
 from . import bp as app
 from flask import render_template, request, redirect, url_for, flash, session
 from app import db
-from app.blueprints.main.models import User, Pokemon
+from app.blueprints.main.models import User, Pokemon, pokemon_user
 from flask_login import current_user, login_required
 
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    return render_template('profile.html.j2', user=current_user)
+    pokemon = Pokemon.query.join(pokemon_user).filter_by(user_id=current_user.id).all()
+    return render_template('profile.html.j2', user=current_user, pokemon=pokemon)
 
 @app.route('/account/', methods=['GET', 'POST'])
 @login_required
 def account():
     return render_template('account.html.j2', user=current_user)
+
+
+@app.route('/pick/<int:id>', methods=['POST'])
+@login_required
+def pick(id):
+    pokemon = Pokemon.query.filter_by(game_id=id).first()
+    try:
+        statement = pokemon_user.insert().values(pokemon_id=id, user_id=current_user.id)
+        db.session.execute(statement)
+        db.session.commit()
+        flash(f'Added {pokemon.name.title()} to your page!', 'success')
+    except Exception as e:
+        flash(f'Error picking {pokemon.name.title()}', 'danger')
+    return redirect(url_for('user.profile'))
